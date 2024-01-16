b6ae0184fab6e9a404a1f95449ab55966d42c11b
Alex Zeng <alex.zenggg@gmail.com>
2024-01-15 22:11:39 -0500

    only styling left to do

--- ToDoList/components/AddTask.js
+++ ToDoList/components/AddTask.js
@@ -1,2 +1,28 @@
 import React, { useState } from 'react'
-import { View, Text, Button, StyleSheet} from 'react-native'
+import { View, TextInput, Button, StyleSheet} from 'react-native'
+
+const AddTask = ({ onAddTask }) => {
+    const [initialInput, setInitialInput] = useState('');
+
+    const handleAddTask = () => {
+        if (initialInput.trim() !== '') {
+          onAddTask(initialInput);
+          setInitialInput('');
+        }
+    };
+
+    return (
+        <View>
+            <TextInput
+                placeholder="Enter Todo Here"
+                value={initialInput}
+                onChangeText={(text) => setInitialInput(text)}
+                keyboardType="default"
+                returnKeyType="done"
+            />
+            <Button title="Add Task" onPress={handleAddTask} />
+        </View>
+    )
+};
+
+export default AddTask;
--- ToDoList/components/ToDoList.js
+++ ToDoList/components/ToDoList.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react'
 import { View, Text, Button, StyleSheet} from 'react-native'
 import 'react-native-get-random-values'
 import { v4 as uuidv4 } from 'uuid'
+import AddTask from './AddTask'
 
 const ToDoList = ({ todoList }) => {
+    // set the state variable with intials values being the prop list
     const [tasks, setTasks] = useState(todoList.map((task) => ({ id: uuidv4(), todo: task})));
 
+    // remove a todo task by only keeping tasks with different ids
     const removeTask = (id) => {
         setTasks(prevTodos => {
             return prevTodos.filter(task => task.id !== id);
         });
     };
+
+    const AddTodo = (task) => {
+        const newTask = {id: uuidv4(), todo: task};
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+    }
 
     return (
         <View>
@@ -20,6 +28,7 @@ const ToDoList = ({ todoList }) => {
                     <Button title='Remove' onPress={() => removeTask(task.id)}/>
                 </View>
             ))}
+            <AddTask onAddTask={AddTodo}></AddTask>
         </View>
     );
 };
